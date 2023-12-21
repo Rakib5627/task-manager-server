bd7873d6d078ef070ed2e1559ef19e5c6589707d
Rakibul Islam <mdrakib.5627@gmail.com>
2023-12-21 23:12:45 +0600

    task related api created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -14,59 +15,86 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y5comcm.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
 
 async function run() {
-  try {
+    try {
 
-    const userCollection = client.db("task-management").collection("users");
-    const reviewCollection = client.db("task-management").collection("reviews");
-   
-    // users related api 
-
-    app.post('/users', async (req, res) => {
-      const user = req.body;
-      const query = { email: user.email }
-      const existingUser = await userCollection.findOne(query);
-      if (existingUser) {
-        return res.send({ message: 'Email already in use', insertedId: null })
-      }
-      const result = await userCollection.insertOne(user);
-      res.send(result);
-    });
-
-    app.get('/users', async (req, res) => {
-      const result = await userCollection.find().toArray();
-      res.send(result);
-    });
+        const userCollection = client.db("task-management").collection("users");
+        const reviewCollection = client.db("task-management").collection("reviews");
+        const taskCollection = client.db("task-management").collection("tasks");
 
 
-    app.get("/users/:id" , async(req , res) => {
-      const id = req.params.id;
-      const query = { _id : new ObjectId(id)};
-      const result = await userCollection.findOne(query);
-      res.send(result);
+        // jwt related api 
 
-    })
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+          })
+
+        // users related api 
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'Email already in use', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
 
 
-    // review related api 
+        app.get("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.findOne(query);
+            res.send(result);
 
-    app.get('/reviews', async (req, res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
-      });
+        })
 
 
-  } finally {
-  }
+        // review related api 
+
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        });
+
+
+        //   task related api 
+
+        app.get('/tasks', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await taskCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/tasks', async (req, res) => {
+            const taskItem = req.body;
+            const result = await taskCollection.insertOne(taskItem);
+            res.send(result);
+        });
+
+
+
+    } finally {
+    }
 }
 
 
@@ -79,9 +107,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
